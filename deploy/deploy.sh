32534d65d41944540f5e5f86566195855e31d230
grahf <contact@grahfmusic.com>
2026-02-22 00:25:08 +1100

    fix: restore missing log functions in deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -46,6 +46,19 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
+# Helper functions
+log_info() {
+    echo -e "${GREEN}[INFO]${NC} $1"
+}
+
+log_error() {
+    echo -e "${RED}[ERROR]${NC} $1"
+}
+
+log_warn() {
+    echo -e "${YELLOW}[WARN]${NC} $1"
+}
+
 # Parse arguments
 while [[ $# -gt 0 ]]; do
     case $1 in
